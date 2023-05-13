02879d9047e1df0818743df0ea8b4ec7f501dfea
Sameer Dey <kalantrilonda@gmail.com>
2023-05-13 16:55:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TipCalculator from './components/TipCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <TipCalculator />
+    </div>
+  );
+}
+
+export default App;
